ee020cb74cb4b7690c4dad663ecf858416134a46
Anish <kapseanish@gmail.com>
2023-07-02 00:21:24 +0530

    enabled safe search + search bar UI complete

--- my-app/src/page/Search.js
+++ my-app/src/page/Search.js
@@ -4,72 +4,72 @@ import TextField from '@mui/material/TextField';
 import ReactDOM from 'react-dom';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import './Search.css';
 
 
 
 const Search = ({ apiKey }) => {
   const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
+  const [searchClicked, setSearchClicked] = useState(false);
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        
-        const response = await fetch(
-          `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&format=json&nojsoncallback=1&text=${query}`
-        );
-        const data = await response.json();
-        const fetchedImages = data.photos.photo.map((photo) => ({
-          id: photo.id,
-          title: photo.title,
-          url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
-        }));
-        setImages(fetchedImages);
-      } catch (error) {
-        console.error('Error fetching images:', error);
-      }
-    };
-
-    if (query) {
-      fetchImages();
-    }
-  }, [apiKey,query]);
-
+  
   const handleSearch = (event) => {
     event.preventDefault();
     setImages([]);
+    setSearchClicked(true);
     if (query) {
       setQuery(query.trim());
     }
   };
 
- 
+  useEffect(() => {
+    if (searchClicked) {
+      const fetchImages = async () => {
+        try {
+          const response = await fetch(
+            `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&format=json&nojsoncallback=1&text=${query}&safe_search=1&per_page=30`
+          );
+          const data = await response.json();
+          const fetchedImages = data.photos.photo.map((photo) => ({
+            id: photo.id,
+            title: photo.title,
+            url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+          }));
+          setImages(fetchedImages);
+        } catch (error) {
+          console.error('Error fetching images:', error);
+        }
+      };
+
+      fetchImages();
+    }
+  }, [apiKey, query, searchClicked]);
+
   return (
     <div>
-      <Typography variant="h2">Search</Typography>
-      <form onSubmit={handleSearch}>
-        <TextField
-          type="text"
-          value={query}
-          onChange={(event) => setQuery(event.target.value)}
-          placeholder="Search for images..."
-        />
-        <Button type="submit">Search</Button>
-      </form>
-      <div style={{ padding: '8%' }}>
-        <ImageList variant="masonry" cols={3} gap={8}>
-          {images.map((image) => (
-            <ImageListItem key={image.id}>
-              <img
-                src={`${image.url}?w=248&fit=crop&auto=format`}
-                srcSet={`${image.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                alt={image.title}
-                loading="lazy"
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </div>
+      <div class="search" style={{ padding: '5%' }}>
+                <form onSubmit={handleSearch}>
+                    <input class="search-bar" type='text' value={query} onChange={(event) => setQuery(event.target.value)} placeholder='Search for images'/>        
+                    <button class="search-button" type='submit'> Search</button>
+                </form>
+        </div>
+      {searchClicked && (
+        <div class="results" style={{ padding: '10%' }}>
+          <ImageList variant="masonry" cols={3} gap={8}>
+            {images.map((image) => (
+              <ImageListItem key={image.id}>
+                <img
+                  src={`${image.url}?w=248&fit=crop&auto=format`}
+                  srcSet={`${image.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={image.title}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </div>
+      )}
     </div>
   );
 };
