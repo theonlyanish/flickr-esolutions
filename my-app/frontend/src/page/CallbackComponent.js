bed9784579a9ee554e655f0d28eefb1d4c10cb1d
Anish <kapseanish@gmail.com>
2023-07-04 00:28:16 +0530

    callback fully functioning

--- my-app/frontend/src/page/CallbackComponent.js
+++ my-app/frontend/src/page/CallbackComponent.js
@@ -1,35 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-class CallbackComponent extends React.Component {
-  componentDidMount() {
+const CallbackComponent = () => {
+  useEffect(() => {
     // using url params for auth
     const urlParams = new URLSearchParams(window.location.search);
     const oauthToken = urlParams.get('oauth_token');
     const oauthVerifier = urlParams.get('oauth_verifier');
 
-    fetch('http://localhost:3000/callback', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        oauthToken,
-        oauthVerifier
-      })
+    fetch(`http://localhost:3000/callback?oauthToken=${oauthToken}&oauthVerifier=${oauthVerifier}`)
+    .then(response => response.text())
+    .then(data => {
+      console.log('OAuth process completed successfully:', data);
+      // Insert React DOM code here
     })
-      .then(response => response.text())
-      .then(data => {
-        console.log('OAuth process completed successfully:', data);
-        //insert react dom here
-      })
-      .catch(error => {
-        console.error('Error completing OAuth:', error);
-      });
-  }
+    .catch(error => {
+      console.error('Error completing OAuth:', error);
+    });
+}, []);
 
-  render() {
-    return <div>Completing OAuth process...</div>;
-  }
-}
+return <div>Completing OAuth process...</div>;
+};
 
 export default CallbackComponent;
